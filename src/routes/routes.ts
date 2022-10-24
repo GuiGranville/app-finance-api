b519184b6116a74a2525b02331e4bd27716e1461
GuiGranville <guigra13@gmail.com>
2022-10-24 13:05:55 -0300

    Adicionando sistema de login e funcionalidades básicas

---
+++ src/routes/routes.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { loginController } from "../controllers/authentication/loginController";
+import { logoutController } from "../controllers/authentication/logoutController";
+import { JwtMiddleware } from "../middlewares/jwt";
+import { cnpjController } from "../service/cnpjController";
+
+import { transactionsRoutes } from "./transactions.routes";
+import usersRoutes from "./users.routes";
+
+const routes = Router();
+
+// Rota para ver se a api está funcionando
+routes.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+// Rota de login
+routes.post("/login", loginController);
+
+// Rotas de usuários
+routes.use("/users", usersRoutes);
+
+// Filtro de autenticação
+routes.use(JwtMiddleware);
+
+// Endpoint para buscar dados do cnpj especificado
+routes.get("/cnpj/:cnpj", cnpjController);
+
+// Rota de logout
+routes.post("/logout", logoutController);
+
+// Rotas de transações
+routes.use("/transactions", transactionsRoutes);
+
+export default routes;
